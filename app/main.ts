356cfa07d756154c3fc1ebcf54dccf76f66c6191
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2025-11-16 16:29:04 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,9 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((socket) => {
+  socket.on("data", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
   socket.on("close", () => {
     socket.end();
   });
