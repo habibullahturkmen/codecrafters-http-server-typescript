be5e542797fef61fd6ad1b6d40297321149c1a79
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2025-11-16 23:10:38 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,5 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// TODO: Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((socket) => {
   socket.on("data", (chunk: Buffer) => {
     const request = chunk.toString();
@@ -37,4 +33,5 @@ const server: net.Server = net.createServer((socket) => {
   });
 });
 
+console.log("Connection Established!");
 server.listen(4221, "localhost");
