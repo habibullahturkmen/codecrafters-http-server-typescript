8df138c3e73986bb4b92a2ca219a4d6ed674762b
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2025-11-16 20:14:53 -0500

    Extract URL path

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (chunk) => {
+    const request = chunk.toString();
+    const [requestLine] = request.split("\r\n");
+    const [method, path] = requestLine.split(" ");
+    console.log(path);
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      return;
+    }
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
   });
   socket.on("close", () => {
     socket.end();
