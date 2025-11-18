dfc9e0aec21f6e328f663fc5f507f22e74e4b768
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2025-11-17 22:05:07 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ const directoryIndex = args.indexOf("--directory");
 const directoryFlag =
   directoryIndex !== -1 ? args[directoryIndex + 1] : "/temp/";
 
-const server: net.Server = net.createServer((socket) => {
+const server: net.Server = net.createServer((socket: net.Socket) => {
   socket.on("data", async (chunk: Buffer) => {
     const request = chunk.toString();
 
@@ -36,12 +36,18 @@ const server: net.Server = net.createServer((socket) => {
     const acceptEncoding: string | undefined = headers
       .find((h) => h.includes("Accept-Encoding"))
       ?.replace("Accept-Encoding: ", "");
+    const connection: string | undefined = headers
+      .find((h) => h.includes("Connection"))
+      ?.replace("Connection: ", "");
     const body: string = request.substring(headerEndIndex + 4, request.length);
 
     switch (method) {
       case "GET":
         if (path === "/") {
-          socket.write(`${httpVersion} 200 OK\r\n\r\n`);
+          socket.write(
+            `${httpVersion} 200 OK\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n`,
+          );
+          handleSocketClosure(connection, socket);
           return;
         }
 
@@ -57,15 +63,17 @@ const server: net.Server = net.createServer((socket) => {
           if (gzip) {
             const gzipped = Bun.gzipSync(text);
             socket.write(
-              `${httpVersion} 200 OK\r\nContent-Encoding: ${gzip}\r\nContent-Type: text/plain\r\nContent-Length: ${gzipped.length}\r\n\r\n`,
+              `${httpVersion} 200 OK\r\nContent-Encoding: ${gzip}\r\nContent-Type: text/plain\r\nContent-Length: ${gzipped.length}\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n`,
             );
             socket.write(gzipped);
+            handleSocketClosure(connection, socket);
             return;
           }
 
           socket.write(
-            `${httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text.length}\r\n\r\n${text}`,
+            `${httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text.length}\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n${text}`,
           );
+          handleSocketClosure(connection, socket);
           return;
         }
 
@@ -78,22 +86,29 @@ const server: net.Server = net.createServer((socket) => {
           const file = Bun.file(`${directoryFlag}${fileName}`);
           const fileContent: string | boolean = await file
             .text()
-            .catch(() => socket.write(`${httpVersion} 404 Not Found\r\n\r\n`));
+            .catch(() =>
+              socket.write(
+                `${httpVersion} 404 Not Found\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n`,
+              ),
+            );
 
           if (typeof fileContent === "boolean") {
+            handleSocketClosure(connection, socket);
             return;
           }
 
           socket.write(
-            `${httpVersion} 200 OK\r\nContent-Type: ${contentType ?? "application/octet-stream"}\r\nContent-Length: ${contentLength ?? fileContent.length}\r\n\r\n${fileContent}`,
+            `${httpVersion} 200 OK\r\nContent-Type: ${contentType ?? "application/octet-stream"}\r\nContent-Length: ${contentLength ?? fileContent.length}\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n${fileContent}`,
           );
+          handleSocketClosure(connection, socket);
           return;
         }
 
         if (path === "/user-agent" || path === "/user-agent/") {
           socket.write(
-            `${httpVersion} 200 OK\r\nContent-Type: ${contentType ?? "text/plain"}\r\nContent-Length: ${contentLength ?? userAgent?.length}\r\n\r\n${userAgent}`,
+            `${httpVersion} 200 OK\r\nContent-Type: ${contentType ?? "text/plain"}\r\nContent-Length: ${contentLength ?? userAgent?.length}\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n${userAgent}`,
           );
+          handleSocketClosure(connection, socket);
           return;
         }
         break;
@@ -109,21 +124,29 @@ const server: net.Server = net.createServer((socket) => {
           const file = Bun.file(`${directoryFlag}${fileName}`);
           const fileContent: string | boolean = await file
             .text()
-            .catch(() => socket.write(`${httpVersion} 404 Not Found\r\n\r\n`));
+            .catch(() =>
+              socket.write(
+                `${httpVersion} 404 Not Found\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n`,
+              ),
+            );
 
           if (typeof fileContent === "boolean") {
             return;
           }
 
           socket.write(
-            `${httpVersion} 201 Created\r\nContent-Type: ${contentType}\r\nContent-Length: ${contentLength}\r\n\r\n${body}`,
+            `${httpVersion} 201 Created\r\nContent-Type: ${contentType}\r\nContent-Length: ${contentLength}\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n${body}`,
           );
+          handleSocketClosure(connection, socket);
           return;
         }
         break;
     }
 
-    socket.write(`${httpVersion} 404 Not Found\r\n\r\n`);
+    socket.write(
+      `${httpVersion} 404 Not Found\r\nConnection: ${connection === "close" ? "close" : "keep-alive"}\r\n\r\n`,
+    );
+    handleSocketClosure(connection, socket);
   });
 
   socket.on("close", () => {
@@ -133,3 +156,10 @@ const server: net.Server = net.createServer((socket) => {
 
 console.log("Connection Established!");
 server.listen(4221, "localhost");
+
+const handleSocketClosure = (
+  connectionStatus: string | undefined,
+  socket: net.Socket,
+) => {
+  if (connectionStatus && connectionStatus === "close") socket.end();
+};
